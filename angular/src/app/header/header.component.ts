f9cd4c9b7b27ae9dfc7b4a9c9da0516c6c935fcd
Kevyn <kahale.ilo@gmail.com>
2017-03-11 18:05:26 -0700

    add responsive menu bar

--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+	showTitle = false;
+
+	toggleTitle(event) {
+		event.stopPropagation()
+		this.showTitle = !this.showTitle;
+		console.log(this.showTitle)
+	}
+
+	// CLose dropdown responsive window when selecting page.
+	@HostListener('click', ['$event.target']) onClick(target) {
+		console.log(target)
+		if (target != "i")
+		{
+			if (this.showTitle)
+			{
+				this.showTitle = !this.showTitle;
+			}
+		}
+  	}
+
+
 
 }
